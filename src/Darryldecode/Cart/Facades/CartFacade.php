b832ae1eb7d6091ed5106f77a9fa86d090fafdef
farhad-rohani2 <138570829+farhad-rohani2@users.noreply.github.com>
2024-08-09 11:36:16 +0330

    Auto Completion  Laravel Facades
    with this change Auto Completion  phpstorm work correctly

--- src/Darryldecode/Cart/Facades/CartFacade.php
+++ src/Darryldecode/Cart/Facades/CartFacade.php
@@ -1,7 +1,13 @@
 <?php namespace Darryldecode\Cart\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Darryldecode\Cart\Cart;
 
+/**
+ * @see Cart
+ * 
+ * Package: Darryldecode\Cart
+ * */
 class CartFacade extends Facade {
 
     protected static function getFacadeAccessor()
